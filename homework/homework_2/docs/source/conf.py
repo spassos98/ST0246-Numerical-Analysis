28f056f11f9bea2bdd68fde07121a98011c2ac06
spassos <spassos547@gmail.com>
2020-03-03 10:22:16 -0500

    Update conf file to considere index.rst as the master doc

--- homework/homework_2/docs/source/conf.py
+++ homework/homework_2/docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Santiago Passos and Alejandro Rojas'
 # The full version, including alpha/beta/rc tags
 release = '1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
